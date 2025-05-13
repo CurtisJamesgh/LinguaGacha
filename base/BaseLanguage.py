814ac7659f58d6ea72fd190dc52cd376986a5738
CurtisJamesgh <bullb0002@gmail.com>
2025-05-14 03:28:13 +0800

    調整 - 繁簡轉換

--- base/BaseLanguage.py
+++ base/BaseLanguage.py
@@ -6,14 +6,14 @@ class BaseLanguage():
         ZH = "ZH"                                          # 中文 (Chinese)
         EN = "EN"                                          # 英文 (English)
         JA = "JA"                                          # 日文 (Japanese)
-        KO = "KO"                                          # 韩文 (Korean)
+        KO = "KO"                                          # 韓文 (Korean)
         RU = "RU"                                          # 阿拉伯文 (Russian)
         AR = "AR"                                          # 俄文 (Arabic)
         DE = "DE"                                          # 德文 (German)
         FR = "FR"                                          # 法文 (French)
-        PL = "PL"                                          # 波兰文 (Polish)
+        PL = "PL"                                          # 波蘭文 (Polish)
         ES = "ES"                                          # 西班牙文 (Spanish)
-        IT = "IT"                                          # 意大利文 (Italian)
+        IT = "IT"                                          # 義大利文 (Italian)
         PT = "PT"                                          # 葡萄牙文 (Portuguese)
         HU = "HU"                                          # 匈牙利文 (Hungrarian)
         TR = "TR"                                          # 土耳其文 (Turkish)
@@ -25,14 +25,14 @@ class BaseLanguage():
         Enum.ZH: {"zh": "中文", "en": "Chinese"},
         Enum.EN: {"zh": "英文", "en": "English"},
         Enum.JA: {"zh": "日文", "en": "Japanese"},
-        Enum.KO: {"zh": "韩文", "en": "Korean"},
+        Enum.KO: {"zh": "韓文", "en": "Korean"},
         Enum.RU: {"zh": "俄文", "en": "Russian"},
         Enum.AR: {"zh": "阿拉伯文", "en": "Arabic"},
         Enum.DE: {"zh": "德文", "en": "German"},
         Enum.FR: {"zh": "法文", "en": "French"},
-        Enum.PL: {"zh": "波兰文", "en": "Polish"},
+        Enum.PL: {"zh": "波蘭文", "en": "Polish"},
         Enum.ES: {"zh": "西班牙", "en": "Spanish"},
-        Enum.IT: {"zh": "意大利文", "en": "Italian"},
+        Enum.IT: {"zh": "義大利文", "en": "Italian"},
         Enum.PT: {"zh": "葡萄牙文", "en": "Portuguese"},
         Enum.HU: {"zh": "匈牙利文", "en": "Hungrarian"},
         Enum.TR: {"zh": "土耳其文", "en": "Turkish"},
